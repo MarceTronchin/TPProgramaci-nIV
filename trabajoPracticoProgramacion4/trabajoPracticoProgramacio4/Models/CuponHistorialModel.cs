a76d7bce1a0539d28554fcea21566ea5e22d22cc
Ariel-Nuñez-11 <tu-ariel_cris2112@hotmail.com>
2025-06-16 21:42:43 -0300

    Cambios en Cupon Historial Model

--- trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Models/CuponHistorialModel.cs
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Models/CuponHistorialModel.cs
@@ -1,27 +1,23 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using trabajoPracticoProgramacion4.Models;
 
 namespace trabajoPracticoProgramacion4.Models
 {
     [Table("Cupones_Historial")]
     public class CuponHistorialModel
     {
-        [Key]
-        public int Id { get; set; }
+        
+        public string NroCupon { get; set; }
 
-        [Required]
-        public int UsuarioId { get; set; }
+        public int Id_Usuario { get; set; }
 
-        [ForeignKey("UsuarioId")]
-        public UsuarioModel Usuario { get; set; }
+        public DateTime FechaUso { get; set; }
 
-        [Required]
-        public int CuponId { get; set; }
+        #region Navegacion
+        public UserModel usuario { get; set; }
+        #endregion
 
-        [ForeignKey("CuponId")]
-        public CuponModel Cupon { get; set; }
-
-        public DateTime FechaUso { get; set; } = DateTime.Now;
     }
 }
