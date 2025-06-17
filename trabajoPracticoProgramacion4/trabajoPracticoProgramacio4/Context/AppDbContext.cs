5956a22bc9ef672a9d639529061eabbef466b735
Marcela <marcelamt.tr@gmail.com>
2025-06-17 17:54:47 -0300

    Corrijo sintaxis en ArticuloModel y actualizo AppDbContext

--- trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Context/AppDbContext.cs
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using trabajoPracticoProgramacio4.Models;
+using trabajoPracticoProgramacion4.Models;
 
 namespace trabajoPracticoProgramacion4.Context
 {
@@ -10,13 +11,28 @@ namespace trabajoPracticoProgramacion4.Context
         {
         }
         public DbSet<UserModel> Usuarios { get; set; }
+        public DbSet<RolModel> Roles { get; set; }
+        public DbSet<CuponModel> Cupones { get; set; }
+        public DbSet<CuponClienteModel> CuponesClientes { get; set; }
+        public DbSet<CuponHistorialModel> CuponesHistorial{ get; set; }
+        public DbSet<ArticuloModel> Articulos { get; set; }
+        public DbSet<TipoCuponModel> TiposCupones { get; set; }
+        public DbSet<CuponDetalle> CuponesDetalles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //clave compuesta de CuponDetalle
+            modelBuilder.Entity<CuponDetalle>()
+                .HasKey(cd => new { cd.CuponId, cd.ArticuloId });
+
+            modelBuilder.Entity<CuponClienteModel>()
+                .HasKey(cc => new(cc.NroCupon, cc.Id_Usuario));
+
+            modelBuilder.Entity<CuponHistorialModel>()
+                .HasKey(ch => new { ch.Id, ch.UsuarioId});
+
             base.OnModelCreating(modelBuilder);
         }
-
-
 
 
     }
--- trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Models/ArticuloModel.cs
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Models/ArticuloModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace trabajoPracticoProgramacion4.Models
 {
-    [Tabel("Articulos")]
+    [Table ("Articulos")]
     public class ArticuloModel
     {
         [Key]
