f3762a4abd94d17edcb6d959631a4a9e5a0e19bf
Marcela <marcelamt.tr@gmail.com>
2025-06-22 23:10:54 -0300

    Agrego nuevamente Helpers/GeneradorCupon

---
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Helpers/GeneradorCupon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace trabajoPracticoProgramacion4.Helpers
+{
+    public static class GeneradorCupon
+    {
+        public static string GenerarCupon()
+        {
+            var codRandom = new Random();
+
+            return $"{codRandom.Next(100, 999)}-{codRandom.Next(100, 999)}-{codRandom.Next(100, 999)}";
+        }
+    }
+}
