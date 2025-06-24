440e3cca48ab7786c8258180941a86b8e4d50fb1
Marcela <marcelamt.tr@gmail.com>
2025-06-24 03:01:53 -0300

    Agrego archivos correspondientes a CuponHistorial

---
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Controllers/CuponHistorialControllercs.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using trabajoPracticoProgramacion4.DTOs;
+using trabajoPracticoProgramacion4.Interfaz;
+using trabajoPracticoProgramacion4.Servicies;
+
+namespace trabajoPracticoProgramacion4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CuponHistorialController : ControllerBase
+    {
+        private readonly ICuponHistorialServices _cuponHistorialService;
+
+        public CuponHistorialController(ICuponHistorialServices cuponHistorialService)
+        {
+            _cuponHistorialService = cuponHistorialService;
+        }
+
+        // POST: api/CuponHistorial
+        [HttpPost]
+        public async Task<IActionResult> RegistrarUso([FromBody] CuponHistorialDto dto)
+        {
+            try
+            {
+                await _cuponHistorialService.RegistrarUsoCupon(dto);
+                return Ok(new { mensaje = "Uso de cupón registrado correctamente." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        // GET: api/CuponHistorial/{idUsuario}
+        [HttpGet("{idUsuario}")]
+        public async Task<IActionResult> VerHistorial(int idUsuario)
+        {
+            var historial = await _cuponHistorialService.ObtenerHistorialPorUsuario(idUsuario);
+            return Ok(historial);
+        }
+    }
+}
---
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/DTOs/CuponHistorialDto.cs
@@ -0,0 +1,12 @@
+﻿namespace trabajoPracticoProgramacion4.DTOs
+{
+    public class CuponHistorialDto
+    {
+
+        public int Id_Cupon { get; set; }
+        public int Id_Usuario { get; set; }
+        public string NroCupon { get; set; }
+      
+
+    }
+}
