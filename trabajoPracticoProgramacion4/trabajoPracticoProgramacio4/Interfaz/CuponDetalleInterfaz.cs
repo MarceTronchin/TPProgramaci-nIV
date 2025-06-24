972166ebdd5d4382d59e6984ff1ea8dfd580e7cb
aalanguerra <galan2736@gmail.com>
2025-06-23 22:40:56 -0300

    Genero CuponDetalleInterfaz

---
+++ trabajoPracticoProgramacion4/trabajoPracticoProgramacio4/Interfaz/CuponDetalleInterfaz.cs
@@ -0,0 +1,17 @@
+using trabajoPracticoProgramacio4.Models;
+using trabajoPracticoProgramacion4.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace trabajoPracticoProgramacion4.Interfaz
+{
+	public interface CuponDetalleInterfaz
+	{
+		Task<List<CuponDetalle>> GetAllDetallesAsync();
+		Task<CuponDetalle?> GetDetalleAsync(int id_Cupon, int idArticulo);
+		Task AddDetalleAsync(DtoCuponDetalle dto);
+		Task UpdateDetalleAsync(DtoCuponDetalle dto);
+		Task DeleteDetalleAsync(int id_Cupon, int idArticulo);
+	}
+}
+
